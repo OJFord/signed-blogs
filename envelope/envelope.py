7c45b3361b3969a8cbbde90e4038e071717ad386
Ollie Ford <me@ojford.com>
2016-10-21 03:36:54 +0100

    Implement list and read

--- envelope/envelope.py
+++ envelope/envelope.py
@@ -3,24 +3,51 @@ Envelope is an AWS-Lambda-driven service that renders log posts signed using
 Keybase.io File System (KBFS).
 """
 import logging
+import requests
+from lxml import html
 
 
-def list_letters(event, _):
+def list_letters(user):
     """Lists all letters available from a user"""
-    return 'Letters from {}'.format(event['user'])
-
-
-def read_letter(event, _):
-    """Retrieves a single letter from a user"""
-    return 'Letter titled {} from {}'.format(
-        event['user'], event['title']
+    response = requests.get(
+        'https://keybase.pub/{user}/envelope'.format(user=user)
     )
+    tree = html.fromstring(response.content)
+    letters = tree.xpath('//div[@class="directory"]//a[@class="file"]/text()')
+
+    return """
+    <html>
+    <head>
+        <title>{user}'s posts</title>
+    </head>
+    <body>
+        <ul>
+            <li>{posts}</li>
+        </ul>
+    </body>
+    </html>
+    """.format(user=user, posts=("</li><li>".join(letters)))
 
 
-def handler(event, ctxt):
+def read_letter(user, title):
+    """Retrieves a single letter from a user"""
+    response = requests.get(
+        'https://{user}.keybase.pub/envelope/{title}'.format(
+            user=user,
+            title=title
+        )
+    )
+    return response.content
+
+
+def handler(event, _):
     """Handle incoming requests from API GW"""
     logging.debug(event)
-    if event.get('title') is not None:
-        return read_letter(event, ctxt)
+
+    title = event.get('title')
+    user = event.get('user')
+
+    if title is not None:
+        return read_letter(user, title)
     else:
-        return list_letters(event, ctxt)
+        return list_letters(user)
