881f19ee227472394f672e57a2e089a1dc9fe17b
Ollie Ford <me@ojford.com>
2016-10-21 01:34:06 +0100

    Use sane defaults for terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,19 @@
 variable "region" {
-    default     = "eu-west-1"
+    description = "AWS region to use"
+    default = "eu-west-1"
 }
 
 variable "aws_profile" {
     description = "Credential profile to use"
+    default = "default"
 }
 
 variable "envelope_pkg" {
     description = "Zip package for deployment to AWS Lambda"
+    default = "lambda.zip"
 }
 
 variable "deploy_stage" {
     description = "Deployment stage - e.g. dev, prod"
+    default = "dev"
 }
